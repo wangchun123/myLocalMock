b9f57220514af78602248aa3d604599813830c9e
qw <123@163.com>
2022-01-22 18:17:16 +0800

    接口支持多文件上传

--- upload.js
+++ upload.js
@@ -43,30 +43,67 @@ app.use(router.routes());
 
 const uploadUrl = "http://localhost:3000/static/upload";
 
-router.post("/upload", (ctx) => {
-  // 上传单个文件
-  const file = ctx.request.files.file; // 获取上传文件
-
-  // 读取文件流
-  const fileReader = fs.createReadStream(file.path);
-
+/*
+ flag: 是否是多个文件上传
+*/
+const uploadFilePublic = function (ctx, files, flag) {
   const filePath = path.join(__dirname, "/static/upload/");
-  // 组装成绝对路径
-  const fileResource = filePath + `/${file.name}`;
+  let fileReader, fileResource, writeStream;
 
-  /*
-   使用 createWriteStream 写入数据，然后使用管道流pipe拼接
-  */
-  const writeStream = fs.createWriteStream(fileResource);
-
-  // 判断 /static/upload 文件夹是否存在，如果不在的话就创建一个
-
-  fileReader.pipe(writeStream);
-  ctx.body = {
-    url: uploadUrl + `/${file.name}`,
-    code: 0,
-    message: "上传成功",
+  const fileFunc = function (file) {
+    // 读取文件流
+    fileReader = fs.createReadStream(file.path);
+    // 组装成绝对路径
+    fileResource = filePath + `/${file.name}`;
+    /*
+       使用 createWriteStream 写入数据，然后使用管道流pipe拼接
+      */
+    writeStream = fs.createWriteStream(fileResource);
+    fileReader.pipe(writeStream);
   };
+  const returnFunc = function (flag) {
+    console.log(flag);
+    console.log(files);
+    if (flag) {
+      let url = "";
+      for (let i = 0; i < files.length; i++) {
+        url += uploadUrl + `/${files[i].name},`;
+      }
+      url = url.replace(/,$/gi, "");
+      ctx.body = {
+        url: url,
+        code: 0,
+        message: "上传成功",
+      };
+    } else {
+      ctx.body = {
+        url: uploadUrl + `/${files.name}`,
+        code: 0,
+        message: "上传成功",
+      };
+    }
+  };
+  if (flag) {
+    // 多个文件上传
+    for (let i = 0; i < files.length; i++) {
+      const f1 = files[i];
+      fileFunc(f1);
+    }
+  } else {
+    fileFunc(files);
+  }
+
+  returnFunc(flag);
+};
+
+router.post("/upload", (ctx) => {
+  let files = ctx.request.files.file;
+  if (files.length === undefined) {
+    // 上传单个文件，它不是数组，只是单个的对象
+    uploadFilePublic(ctx, files, false);
+  } else {
+    uploadFilePublic(ctx, files, true);
+  }
 });
 
 app.listen(3000, () => {
